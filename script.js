20dcbe943d3d2f48cb16081bf6ce9c313b66feda
seanmc114 <smcquaid@syngestreet.com>
2026-03-04 01:11:09 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,29 +1,59 @@
 const playBtn = document.getElementById("playBtn");
-const audio = document.getElementById("audio");
+const audio1 = document.getElementById("audio1");
+const audio2 = document.getElementById("audio2");
 const input = document.getElementById("codeInput");
 const responseText = document.getElementById("responseText");
 
 playBtn.onclick = () => {
-  audio.play();
-  playBtn.style.display = "none";
+
+audio1.play();
+
+playBtn.style.display = "none";
+
 };
 
-audio.onended = () => {
-  input.style.display = "block";
-  input.focus();
+audio1.onended = () => {
+
+setTimeout(() => {
+
+audio2.play();
+
+},1500);
+
 };
 
-input.addEventListener("keydown", function(e) {
-  if (e.key === "Enter") {
-    const value = input.value.trim().toUpperCase();
+audio2.onended = () => {
 
-    if (value === "CONNECT") {
-      responseText.innerText = "we hear you.";
-      responseText.style.opacity = 1;
+input.style.display = "block";
+input.focus();
 
-      setTimeout(() => {
-        responseText.innerText = "is it happening now?";
-      }, 5000);
-    }
-  }
+};
+
+input.addEventListener("keydown",function(e){
+
+if(e.key==="Enter"){
+
+const value=input.value.trim().toUpperCase();
+
+if(value==="CONNECT"){
+
+responseText.innerText="we hear you.";
+responseText.style.opacity=1;
+
+setTimeout(()=>{
+
+responseText.innerText="is it happening now?";
+
+},5000);
+
+setTimeout(()=>{
+
+responseText.innerText="send something back.";
+
+},11000);
+
+}
+
+}
+
 });
